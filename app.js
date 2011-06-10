d5db9145bef71c0a63b1cf540210ed6d0bb5bd76
Freek Wielstra <fr.wielstra@gmail.com>
2011-06-10 10:19:17 +0200

    Moved environment configuration to environment.js and routes to routes.js, neatening things up.

--- app.js
+++ app.js
@@ -4,35 +4,10 @@
  */
 
 var express = require('express');
-
 var app = module.exports = express.createServer();
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
-});
+require('./config/environment.js')(app, express);
+require('./config/routes.js')(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d", app.address().port);
---
+++ config/environment.js
@@ -0,0 +1,18 @@
+module.exports = function(app, express) {
+    app.configure(function(){
+      app.set('views', __dirname + '/../views');
+      app.set('view engine', 'jade');
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(app.router);
+      app.use(express.static(__dirname + '/../public'));
+    });
+    
+    app.configure('development', function(){
+      app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    });
+    
+    app.configure('production', function(){
+      app.use(express.errorHandler()); 
+    });
+};
