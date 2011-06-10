12051f6dcb46a228f879ec116e08046dbc18efeb
Freek Wielstra <fr.wielstra@gmail.com>
2011-06-10 14:51:26 +0200

    Simplified the thread request handlers, got rid of logging, removed duplicated and obsolete tests.

--- controllers/thread.js
+++ controllers/thread.js
@@ -1,19 +1,11 @@
 var Thread = require('../models/thread.js');
 exports.post = function(req, res) {
-    var newThread = new Thread({title: req.body.title, author: req.body.author});
-    newThread.save();
-    console.log('New thread ' + newThread.title + ' saved!');
+    new Thread({title: req.body.title, author: req.body.author}).save();
     res.redirect('home');
 }
 
 exports.show = (function(req, res) {
     Thread.findOne({title: req.params.title}, function(error, article){
-        if (error) {
-            console.log(error);
-            res.send(error);
-        } else {
-            console.log('Returning article ', article);
-            res.send(article);
-        }
+        res.send((error) ? error : article);
     })
 });
