809a3bf2c2325656d67b83ea6e0610c5acae98bc
Freek Wielstra <fr.wielstra@gmail.com>
2011-06-11 19:24:46 +0200

    Added a bit of style as well as links to threads. This concludes part 1 of the Sammy tutorial.

--- public/js/app.js
+++ public/js/app.js
@@ -13,11 +13,19 @@ var testdata = {
 		this.element_selector = '#main';
 
 		this.get('#/', function(context) {
+			context.app.swap(''); // clears the screen when someone presses back to return to #/.
 			this.load('thread')
 			.then( function(threads) {
 				$.each(threads, function(i, thread) {
 					context.render('templates/thread.template', {thread : thread}).appendTo(context.$element());
 				});
+			});
+		});
+		
+		this.get('#/thread/:title', function(context) {
+			this.load('thread/' + this.params['title'])
+			.then(function(thread) {
+				context.partial('templates/thread.template', {thread: thread});
 			});
 		});
 	});
