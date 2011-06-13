344efd6478e9bc2096655dd6b95d11300062cc59
Freek Wielstra <fr.wielstra@gmail.com>
2011-06-13 19:34:58 +0200

    Automated the testdata insertion process. Will do for now.

--- testdata.js
+++ testdata.js
@@ -1,16 +1,34 @@
 // Some test data, execute these commands in the Mongo console to have some
 // data in there.
 
+var mongoose = require('mongoose');
+var connection = mongoose.connect('mongodb://localhost/norum');
 
-db.threads.save({
+var Thread = require('./models/thread.js')(connection);
+var Post = require('./models/post.js')(connection);
+
+var thread = new Thread({
   postdate : "Sun Jun 12 2011 14:35:26 GMT+0200 (CEST)",
   author : "henk",
   title : "test-2"
 });
 
-db.threads.find();
-// copy / paste the objectid of the inserted thread and chuck it into these posts.
-var now = new Date();
-db.posts.save({thread: ObjectId("4df646a9875a5d23ed8df40c"), postdate: now, author: 'Hodor', post: 'Hodor hodor hodor'});
-db.posts.save({thread: ObjectId("4df646a9875a5d23ed8df40c"), postdate: now, author: 'Hodor', post: 'Hodor, hodor hodor hodor.'});
-db.posts.save({thread: ObjectId("4df646a9875a5d23ed8df40c"), postdate: now, author: 'Hodor', post: 'Hodor! HODOR HODOR HODOR HODOR!'});
+// hacky.
+var postsSaved = 0;
+var callback = function() {
+  postsSaved++;
+  if (postsSaved == 3) {
+    console.log('All done, added a new thread and posts:', thread);
+    process.exit(0);
+  }
+}
+
+thread.save(function(error) {
+  var now = new Date();
+  new Post({thread: thread._id, postdate: now, author: 'Hodor', post: 'Hodor hodor hodor.'}).save(callback);
+  new Post({thread: thread._id, postdate: now, author: 'Hodor', post: 'Hodor, hodor hodor hodor.'}).save(callback);
+  new Post({thread: thread._id, postdate: now, author: 'Hodor', post: 'Hodor! HODOR HODOR HODOR HODOR!'}).save(callback);
+});
+
+
+
