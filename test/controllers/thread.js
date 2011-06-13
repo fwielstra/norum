6248c87623c3e0137837f7fca255de626839c433
Freek Wielstra <fr.wielstra@gmail.com>
2011-06-13 14:03:07 +0200

    Upgraded NodeUnit to 0.2.3 and fixed the thread.js test with a .followRedirect(false).
    More info: https://github.com/indexzero/api-easy/issues/8

--- test/controllers/thread.js
+++ test/controllers/thread.js
@@ -6,12 +6,8 @@ require('../../app.js');
 exports.thread = suite
     .use('localhost', 3000)
     .setHeader('Content-Type', 'application/json')
+    .followRedirect(false)
     .discuss('When using the Thread API')
-        .get('/')
-            .expect(200, {message: 'y halo thar'})
-            .next()
         .post('/thread', {title: 'title-test', author: 'John Doe'})
-            // TODO: it should actually return a 302, but it seems
-            // apieasy does a POST on the url redirected to.
-            .expect(404)
+            .expect(302)
             .next();
